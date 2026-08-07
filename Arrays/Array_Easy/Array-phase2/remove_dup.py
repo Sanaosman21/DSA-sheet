5f6af6a5f206237bd509f7d6caaaeae61119fb5e
Sanaosman21 <sanaosman8922@gmail.com>
2026-08-07 15:48:34 +0530

    arrays

---
+++ Arrays/Array_Easy/Array-phase2/remove_dup.py
@@ -0,0 +1,11 @@
+def fun(arr):
+    i=0
+    for j in range(1,len(arr)):
+        if arr[i]!=arr[j]:
+            i+=1
+            arr[i]=arr[j]
+    return i+1
+arr=[1, 1, 2, 2, 3]
+k=fun(arr)
+print(k)
+print("unique elements are",arr[:k])
