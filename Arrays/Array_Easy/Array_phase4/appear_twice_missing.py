76d0ba79ef2f1e0771bdc6e0ca378b49cb112156
Sanaosman21 <sanaosman8922@gmail.com>
2026-08-15 12:00:22 +0530

    array phase 4

--- Arrays/Array_Easy/Array_phase4/appear_twice_missing.py
+++ Arrays/Array_Easy/Array_phase4/appear_twice_missing.py
@@ -1,16 +1,32 @@
-#brute force
-def fun(nums):
+# #brute force
+# def fun(nums):
+#     n=len(nums)
+#     freq=[0]*(n+1)
+#     for num in nums:
+#             freq[num]+=1
+#     repeating=-1
+#     missing=-1
+#     for i in range(1,n+1):
+#         if freq[i]==2:
+#             repeating=i
+#         elif freq[i]==0:
+#             missing=i
+#     return [missing , repeating ]
+# nums=[1,2,2,4]
+# print(fun(nums))
+
+#optimal 
+def RM(nums):
     n=len(nums)
-    freq=[0]*(n+1)
-    for num in nums:
-            freq[num]+=1
-    repeating=-1
-    missing=-1
-    for i in range(1,n+1):
-        if freq[i]==2:
-            repeating=i
-        elif freq[i]==0:
-            missing=i
-    return [missing , repeating ]
+    E=n*(n+1)//2
+    A=sum(nums)
+    diff=A-E
+    E_sqr=n*(n+1)*(2*n+1)//6
+    A_sqr=sum(num*num for num in nums)
+    sqr_diff=A_sqr-E_sqr
+    sum_rm=sqr_diff//diff
+    R=(diff+sum_rm)//2
+    M=sum_rm-R
+    return [R,M]
 nums=[1,2,2,4]
-print(fun(nums))
+print(RM(nums))
